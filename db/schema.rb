8b8ffc0bdb0a7cca58c5caad14d049c2f86f4794
Ryan Palermo <stix250@gmail.com>
2016-08-12 17:15:15 -0400

    add school model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812184552) do
+ActiveRecord::Schema.define(version: 20160812211243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20160812184552) do
   create_table "crimes", force: :cascade do |t|
     t.float    "latitude"
     t.float    "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "schools", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "district"
+    t.float    "score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
