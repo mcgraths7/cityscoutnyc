91aa3823419ab8c492fbebe2ea60eaa2e8ccf4d9
Steven McGrath <mcgrath.steven7@gmail.com>
2016-08-15 14:32:32 -0400

    parks and new subway data

--- app/models/park.rb
+++ app/models/park.rb
@@ -6,8 +6,5 @@ class Park < ApplicationRecord
     # SELECT name, address, ( 6371 * acos( cos( radians(latitudeUserInput) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(longitudeUserInput) ) + sin( radians(latitudeUserInput) ) * sin( radians( latitude ) ) ) ) AS distance FROM your_table_here HAVING distance <= 2 ORDER BY distance;
   end
 
-  def find_by_name(park_name)
-    self.where('name = ?', park_name)
-  end
 
 end
