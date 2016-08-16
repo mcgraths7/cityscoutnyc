d45933ec76b5d0d5b6bf7566d036ac7933ee6cf8
Steven McGrath <mcgrath.steven7@gmail.com>
2016-08-16 17:44:58 -0400

    fixed filter order

--- app/models/crime.rb
+++ app/models/crime.rb
@@ -6,6 +6,6 @@ class Crime < ApplicationRecord
   end
 
   def self.filter_by_district(lat_max, lat_min, long_max, long_min)
-    Crime.where('crimes.longitude < ? AND crimes.longitude > ? AND crimes.latitude < ? AND crimes.latitude > ? ',lat_max, lat_min, long_max, long_min )
+    Crime.where('crimes.latitude < ? AND crimes.latitude > ? AND crimes.longitude < ? AND crimes.longitude > ? ',lat_max, lat_min, long_max, long_min )
   end
 end
