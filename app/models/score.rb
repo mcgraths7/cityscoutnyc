93d4529a97bcf0247572cf6170c72d3dba0ddaf0
Ryan Palermo <stix250@gmail.com>
2016-08-16 22:17:18 -0400

    added averages

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,7 +1,18 @@
 class Score < ApplicationRecord
 
-  def average(category)
+  def get_average(category)
     vals = VotingDistrict.all.pluck(category)
     vals.inject(0.0) {|result, num| result + num } / vals.size
   end
+
+  def averages
+    {total_score: 0, safety: get_average(:crime), education: get_average(:schools), transportation: (get_average(:bikes) + get_average(:accidents) + get_average(:subways)) / 3, recreation: get_average(:parks)}
+  end
 end
+
+# t.integer "total_score"
+# t.integer "safety_score"
+# t.integer "education_score"
+# t.integer "transportation_score"
+# t.integer "recreation_score"
+# t.integer "cost_of_living_score"
