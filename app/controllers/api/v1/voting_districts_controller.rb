1a5decc561b73bf8b2ae695937c441e69c0595e0
Steven McGrath <mcgrath.steven7@gmail.com>
2016-09-19 18:58:09 -0400

    fixed averages

--- app/controllers/api/v1/voting_districts_controller.rb
+++ app/controllers/api/v1/voting_districts_controller.rb
@@ -6,12 +6,11 @@ module Api
           point = UserPoint.create(voting_districts_params)
           district = VotingDistrict.find_correct_district(point.geocode[0], point.geocode[1])
           render json: {own_score: Score.own_scores(district),
-            averages = {
+            averages: {
               :accidents=>160.55194245292316,
               :bikes=>5,
               :crime=>321.1038849058463,
-              :parks=>57,
-              :restaurants=>53,
+              :parks=>57, :restaurants=>53,
               :subways=>4
             },
             attributes: district.attributes,
