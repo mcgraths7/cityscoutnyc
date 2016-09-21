33ded5194dd64193395fe756f613cd9473072b57
Steven McGrath <mcgrath.steven7@gmail.com>
2016-09-21 11:21:05 -0400

    added cors

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,10 @@ Bundler.require(*Rails.groups)
 
 module CityScout
   class Application < Rails::Application
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => 'https://cityscout-nyc.herokuapp.com/',
+      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
